7372af830012b7966fdb750504e335201b255dc4
Ryan <ryan1858.rs@hotmail.com>
2020-07-15 19:38:25 -0300

    Configurei o Projeto

---
+++ BattleShip.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : BattleShip.cpp
+// Author      : soul-a & Luana
+// Version     :
+// Copyright   : 
+// Description : A Battle Ship game for studing programing and algorithms
+//============================================================================
+
+/*
+ * Bibliotecas
+ */
+#include <iostream>
+
+/*
+ * Função principal
+ */
+int main(int argc, char **argv) {
+
+	return 0;
+}
