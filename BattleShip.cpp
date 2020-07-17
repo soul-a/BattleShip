ff714cd040ac507fb8be7c5f865ef6bc95c96a7a
Ryan <ryan1858.rs@hotmail.com>
2020-07-16 21:09:24 -0300

    Molde da matriz do projeto

--- BattleShip.cpp
+++ BattleShip.cpp
@@ -10,11 +10,52 @@
  * Bibliotecas
  */
 #include <iostream>
+#include <stdio.h>
+
+
+/*
+ * Assinaturas e Prototipos
+ */
+void CriarTabuleiro();
+
+
+/*
+ * Variaveis globais
+ */
+
 
 /*
  * Função principal
  */
 int main(int argc, char **argv) {
 
+	CriarTabuleiro();
+
 	return 0;
+}
+
+void CriarTabuleiro(){
+
+	char tabuleiro[14][14];
+
+	// percorrer as linhas da matriz
+		for(int i = 0; i < 14; i++) {
+
+			// percorrer as colunas
+			for(int j = 0; j < 14; j++){
+
+				tabuleiro[i][j] = '~';
+			}
+		}
+
+		// percorrer as linhas da matriz
+			for(int i = 0; i < 14; i++) {
+
+				// percorrer as colunas
+				for(int j = 0; j < 14; j++){
+
+					std::cout << tabuleiro[i][j] << " | ";
+				}
+				std::cout << "" << std::endl;
+			}
 }
